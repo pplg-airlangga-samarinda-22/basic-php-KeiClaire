6dd68ff5a52e2b63baea00dafb2d9bb27c6e104a
Rizal Dwi Ristiadi <142187458+KeiClaire@users.noreply.github.com>
2025-01-07 09:02:47 +0800

    selesai

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+    // 
+    $nama = "Rizal Dwi Ristiadi";
+    $nisn = "0072757957";
 
+    // tampilkan/cetak variabel tersebut ke layar
+    echo 'Nama saya adalah '. $nama .' dan NISN saya '. $nisn;
 ?>
